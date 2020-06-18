d72671f3c64c4b6bf71b0fc7a423b75defc55370
Ben Schwartz <ben.schwartz@kasasa.com>
2020-06-18 00:35:24 -0500

    adding factory and fluent assimilation

---
+++ src/main/Fliglio/Borg/AmqpCollectiveFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Fliglio\Borg;
+
+
+use Fliglio\Borg\Amqp\AmqpCollectiveDriver;
+use Fliglio\Borg\Mapper\DefaultMapper;
+
+class AmqpCollectiveFactory {
+	private $rabbitConnection;
+	private $routingNamespace;
+	public function __construct($rabbitConnection, $routingNamespace) {
+		$this->rabbitConnection = $rabbitConnection;
+		$this->routingNamespace = $routingNamespace;
+	}
+
+	public function create($instancesToAssimilate = []) {
+		$driver  = new AmqpCollectiveDriver($this->rabbitConnection);
+		$mapper  = new DefaultMapper($driver);
+		$routing = new RoutingConfiguration($this->routingNamespace);
+
+		$coll = new Collective($driver, $mapper, $routing);
+		foreach ($instancesToAssimilate as $inst) {
+			$coll->assimilate($inst);
+		}
+		return $coll;
+	}
+}
+
